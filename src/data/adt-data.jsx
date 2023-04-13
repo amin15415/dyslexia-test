b2729a536c24f7a0dda0ef67410b1c06b1996c13
Griff <griff1293@gmail.com>
2023-04-13 13:05:12 -0500

    add adt words

---
+++ src/data/adt-data.jsx
@@ -0,0 +1,15 @@
+export const adtWords = [
+    { level: '1', words: { 'father': null, 'could': null, 'know': null, 'money': null, 'call': null, 'funny': null, 'there': null } },
+    { level: '2', words: { 'does': null, 'listen': null, 'city': null, 'animal': null, 'light': null, 'uncle': null, 'rolled': null } },
+    { level: '3', words: { 'calf': null, 'enough': null, 'meadow': null, 'heavy': null, 'business': null, 'believe': null, 'laugh': null } },
+    { level: '4', words: { 'delight': null, 'familiar': null, 'rough': null, 'glisten': null, 'league': null, 'spectacles': null, 'decorate': null } },
+    { level: '5', words: { 'cautious': null, 'ancient': null, 'toughen': null, 'height': null, 'doubt': null, 'position': null, 'contagious': null } },
+    { level: '6', words: { 'conceited': null, 'foreign': null, 'knapsack': null, 'decisions': null, 'allegiance': null, 'leisure': null, 'deny': null } },
+    { level: 'JRH', words: { 'dominion': null, 'intrigue': null, 'aeronautic': null, 'trudge': null, 'tomorrow': null, 'graciously': null, 'bridge': null } },
+    { level: 'HIS', words: { 'pollute': null, 'exonerate': null, 'risible': null, 'regime': null, 'endeavor': null, 'islet': null, 'heinous': null } },
+    { level: 'LOD', words: { 'parliament': null, 'gnostic': null, 'mannequin': null, 'homologous': null, 'prerequisite': null, 'rhapsody': null, 'euphony': null } },
+    { level: 'UPD', words: { 'litigious': null, 'tincture': null, 'oligarchy': null, 'inefficacious': null, 'demagogue': null, 'parturition': null, 'mimicry': null } },
+    { level: 'MAS', words: { 'homeopathy': null, 'evanesce': null, 'geodesy': null, 'coulomb': null, 'zoophyte': null, 'execrable': null, 'triptych': null } },
+    { level: 'DOC', words: { 'sobriquet': null, 'deliquesce': null, 'colloquy': null, 'vitiate': null, 'sycophant': null, 'intermezzo': null, 'dehiscence': null } },
+    { level: 'POD', words: { 'exiguous': null, 'malapropos': null, 'ytterbium': null, 'monocotyledon': null, 'leitmotif': null, 'egregious': null, 'legerdemain': null } },
+  ];
