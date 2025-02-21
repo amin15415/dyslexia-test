4517140f19e6da89e21db49b8b48032960707453
amin@inoox.com <amin@inoox.com>
2025-02-21 21:43:56 +0330

    - changes in eidetic and phonetic tests to include a tutorial before them and also changed the header of them - added a boice check before eidetic

--- src/pages/Eidetic/Eidetic.jsx
+++ src/pages/Eidetic/Eidetic.jsx
@@ -1,8 +1,12 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { getEideticWords } from '../../utils/getEncodingWords';
 import { useNavigate } from 'react-router-dom';
 import { useSessionStorage } from '../../hooks/useSessionStorage';
 import "./Eidetic.css";
+import { IconButton, Stack, Typography } from '@mui/material';
+import { ReactTyped } from "react-typed";
+import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+import PauseCircleOutlineIcon from '@mui/icons-material/PauseCircleOutline';
 
 const Eidetic = () => {
     const navigate = useNavigate();
@@ -15,10 +19,56 @@ const Eidetic = () => {
                                                             testWords
                                                             );
     const audioPaths = eideticWords.map((word) => require(`../../assets/audio/${word}.mp3`));
+    const samplePath = require(`../../assets/audio/laugh.mp3`);
+    const instruction1Path = require(`../../assets/audio/eidetic-instruction-1.mp3`);
+    const instruction2Path = require(`../../assets/audio/eidetic-instruction-2.mp3`);
+    const voiceTestPath = require(`../../assets/audio/eidetic-voice-test.mp3`);
+
     const [userInputs, setUserInputs] = useState(Array(audioPaths.length).fill(''));
     const [incompleteSubmit, setIncompleteSubmit] = useState(false);
     const [currentItem, setCurrentItem] = useState(0);
 
+    const [isTutorial, setIsTutorial] = useState(true);
+    const [isReadyToStart, setIsReadyToStart] = useState(false);
+    const [animationPhase, setAnimationPhase] = useState(0);
+    const [inputTyped,setInputTyped] = useState();
+    const [instruction1Typed,setInstruction1Typed] = useState();
+    const [instruction2Typed,setInstruction2Typed] = useState();
+
+    const [sampleInputValue,setSampleInputValue] = useState('');
+    const [isPlaying,setIsPlaying] = useState(false);
+    const [isAnimating,setIsAnimating] = useState(false);
+
+    const sampleAudioRef = useRef();
+    const instructionAudioRef1 = useRef();
+    const instructionAudioRef2 = useRef();
+    const playButtonRef = useRef();
+    const voiceTestButton = useRef();
+
+    useEffect(() => {
+      // if first phase is in place the audio for first instruction should be started
+      if (animationPhase == 1) { instructionAudioRef1.current.play(); }
+      if (animationPhase == 2) {
+        sampleAudioRef.current.play();
+        playButtonRef.current.classList.add('dynamic-play-button');
+        setTimeout(() => playButtonRef.current.classList.remove('dynamic-play-button'), 200);
+      }
+      if (animationPhase == 3) {
+        instructionAudioRef2.current.play();
+        // instruction2Typed.start(); 
+      }
+      if (animationPhase == 4)  inputTyped.start()
+    }, [animationPhase]);
+
+    useEffect(() => {
+      if (isPlaying && !isAnimating && animationPhase == 1)  instruction1Typed.start();
+      if (isPlaying && !isAnimating && animationPhase == 3)  instruction2Typed.start();
+      if (!isPlaying && !isAnimating && animationPhase > 0 && animationPhase < 5) {
+        setAnimationPhase(animationPhase + 1);
+      }
+    }, [isPlaying, isAnimating]);
+
+    
     const handleSubmit = () => {
       if (userInputs[currentItem] === '') {
         setIncompleteSubmit(true);
@@ -63,9 +113,112 @@ const Eidetic = () => {
               </div>
             ) : (
               <>
+                { isTutorial &&
+                <Stack justifyContent="center" alignItems="center" spacing={2}>
+                  <Typography variant='h4'>Spelling Test Tutorial</Typography>
+
+                  { (!isReadyToStart || animationPhase == 0) &&
+                    <Stack justifyContent="center" alignItems="center" spacing={1}>
+                      <Typography variant='h6'>Please ensure your audio is functioning and that your device is not muted.</Typography>
+                      { !isPlaying &&
+                      <IconButton onClick={() => voiceTestButton.current.play()}>
+                        <PlayCircleOutlineIcon sx={{width: "100px", height: "100px", color: "#FFC107"}} />
+                      </IconButton>
+                      }
+                      { isPlaying &&
+                      <IconButton onClick={() => voiceTestButton.current.pause()}>
+                        <PauseCircleOutlineIcon sx={{width: "100px", height: "100px", color: "#FFC107"}} />
+                      </IconButton>
+                      }
+                      <Typography variant='h6'>Click on the play button and continue if you heard the voice clearly.</Typography>
+                      
+                      <audio ref={voiceTestButton} src={voiceTestPath} onPlay={()=> setIsPlaying(true)} onPause={()=> setIsPlaying(false)} onEnded={()=> {setIsReadyToStart(true); setIsPlaying(false);}} />
+                    </Stack>
+                  }
+ 
+                  { isReadyToStart && animationPhase == 0 &&
+                    <button onClick={() => setAnimationPhase(1)}>
+                      I Heard The Voice
+                    </button>
+                  }
+
+                  <ReactTyped
+                        typedRef={setInstruction1Typed}
+                        strings={["First you will hear a word."]}
+                        typeSpeed={30}
+                        className='tutorial-text'
+                        stopped
+                        showCursor={false}
+                        onStart={() => setIsAnimating(true)}
+                        onComplete={() => setIsAnimating(false)}
+                  />
+
+                  <audio ref={instructionAudioRef1} src={instruction1Path} onPlay={()=> setIsPlaying(true)} onEnded={()=> setIsPlaying(false)} />
+                  { animationPhase > 0 &&
+                  <div style={{ position: 'relative', display: 'inline-block' }}>
+                    <audio ref={sampleAudioRef} src={samplePath} controls style={{ position: 'relative', zIndex: 1 }} onPlay={()=> setIsPlaying(true)} onEnded={()=> setIsPlaying(false)}/>
+                    <div
+                      ref={playButtonRef}
+                      style={{
+                        position: 'absolute',
+                        top: '0',
+                        left: '0',
+                        right: '0',
+                        bottom: '0',
+                        zIndex: animationPhase > 3 ? -1 : 2,
+                      }}
+                    ></div>
+                  </div>
+                  }
+                  <ReactTyped
+                        typedRef={setInstruction2Typed}
+                        strings={['You can replay this word by pressing the play button. <br> Next you should spell this word exactly how you would spell it on a spelling test. <br> Please spell the word "laugh" exactly as demonstrated.']}
+                        typeSpeed={30}
+                        className='tutorial-text'
+                        stopped
+                        showCursor={false}
+                        onStart={() => setIsAnimating(true)}
+                        onComplete={() => setIsAnimating(false)}
+                  />
+                  <audio ref={instructionAudioRef2} src={instruction2Path} onPlay={()=> setIsPlaying(true)} onEnded={()=> setIsPlaying(false)} />
+                  
+                  { animationPhase > 2 &&
+                  <ReactTyped
+                    typedRef={setInputTyped}
+                    strings={["laugh",""]}
+                    typeSpeed={250}
+                    backSpeed={70}
+                    attr="value"
+                    stopped
+                    onStart={() => setIsAnimating(true)}
+                    onComplete={() => setIsAnimating(false)}
+                  >
+                    <input
+                      type="text"
+                      placeholder="Enter Spelling"
+                      value={sampleInputValue}
+                      spellCheck={false}
+                      autoCorrect="off"
+                      autoComplete="off"
+                      autoCapitalize="none"
+                      inputMode="none"
+                      autoFocus
+                      onChange={(e) => setSampleInputValue(e.target.value)}
+                    />
+                  </ReactTyped>
+                  }
+                  { animationPhase > 4 &&
+                    <button disabled={sampleInputValue.toLowerCase().trim() !== "laugh" } onClick={() => setIsTutorial(false)}>
+                      Start The Test
+                    </button>
+                  }
+                </Stack>
+                }
+                { !isTutorial &&
+                  <>
                 <div>
-                  <h1>Spell these words exactly as they should be spelled</h1>
-                  <p>For instance, laugh should be spelled 'laugh'.</p>
+                  <h1>Spell these words correctly, like on a spelling test.</h1>
+                  {/* <p>For instance, laugh should be spelled 'laugh'.</p> */}
                 </div>
                 <form onSubmit={(e) => { e.preventDefault(); handleSubmit(); }}>
                   <div>
@@ -100,7 +253,9 @@ const Eidetic = () => {
                     </button>
                   </div>
                 </form>
-              </>
+                </>
+                }
+                </>
             )}
           </div>
         </div>
--- src/pages/Phonetic/Phonetic.jsx
+++ src/pages/Phonetic/Phonetic.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { getPhoneticWords } from "../../utils/getEncodingWords";
 import correctPhoneticWords from "../../utils/phoneticCorrect";
 import { useSessionStorage } from '../../hooks/useSessionStorage';
 import { useNavigate } from "react-router-dom";
+import { ReactTyped } from "react-typed";
 import "./Phonetic.css";
+import { Stack, Typography } from "@mui/material";
 
 const Phonetic = () => {
   const navigate = useNavigate();
@@ -15,11 +17,56 @@ const Phonetic = () => {
   const audioPaths = phoneticWords.map((word) =>
     require(`../../assets/audio/${word}.mp3`)
   );
+
+  const samplePath = require(`../../assets/audio/laugh.mp3`);
+  const instruction1Path = require(`../../assets/audio/phonetic-instruction-1.mp3`);
+  const instruction2Path = require(`../../assets/audio/phonetic-instruction-2.mp3`);
+
   const [userInputs, setUserInputs] = useState(
     Array(audioPaths.length).fill("")
   );
   const [incompleteSubmit, setIncompleteSubmit] = useState(false);
   const [currentItem, setCurrentItem] = useState(0);
+
+
+  const [isTutorial, setIsTutorial] = useState(true);
+  const [animationPhase, setAnimationPhase] = useState(0);
+  const [inputTyped,setInputTyped] = useState();
+  const [instruction1Typed,setInstruction1Typed] = useState();
+  const [instruction2Typed,setInstruction2Typed] = useState();
+
+  const [sampleInputValue,setSampleInputValue] = useState('');
+  const [isPlaying,setIsPlaying] = useState(false);
+  const [isAnimating,setIsAnimating] = useState(false);
+
+  const sampleAudioRef = useRef();
+  const instructionAudioRef1 = useRef();
+  const instructionAudioRef2 = useRef();
+  const playButtonRef = useRef();
+
+  useEffect(() => {
+    // if first phase is in place the audio for first instruction should be started
+    if (animationPhase == 1) { instructionAudioRef1.current.play(); }
+    if (animationPhase == 2) {
+      sampleAudioRef.current.play();
+      playButtonRef.current.classList.add('dynamic-play-button');
+      setTimeout(() => playButtonRef.current.classList.remove('dynamic-play-button'), 200);
+    }
+    if (animationPhase == 3) {
+      instructionAudioRef2.current.play();
+      // instruction2Typed.start(); 
+    }
+    if (animationPhase == 4)  inputTyped.start()
+  }, [animationPhase]);
+
+  useEffect(() => {
+    if (isPlaying && !isAnimating && animationPhase == 1)  instruction1Typed.start();
+    if (isPlaying && !isAnimating && animationPhase == 3)  instruction2Typed.start();
+    if (!isPlaying && !isAnimating && animationPhase > 0 && animationPhase < 5) {
+      setAnimationPhase(animationPhase + 1);
+    }
+  }, [isPlaying, isAnimating]);
+
 
   const handleSubmit = () => {
     if (userInputs[currentItem] === "") {
@@ -59,53 +106,138 @@ const Phonetic = () => {
 
   return (
     <div className="encoding-container" key={currentItem}>
-      <div>
-        <h1>Spell these words exactly like they sound</h1>
-        <p>For instance, laugh should be spelled 'laf'.</p>
-      </div>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          handleSubmit();
-        }}
-      >
-        <div>
-          {currentItem === 0 && (
-            <div>
-              <h3>Press Play</h3>
-            </div>
-          )}
-          <audio
-            src={audioPaths[currentItem]}
-            controls
-            autoPlay={currentItem !== 0}
+      { isTutorial &&
+        <Stack justifyContent="center" alignItems="center" spacing={2}>
+          <Typography variant='h4'>Phonetic Test Tutorial</Typography>
+          { animationPhase == 0 &&
+            <button onClick={() => setAnimationPhase(1)}>
+              Start Test
+            </button>
+          }
+
+          <ReactTyped
+                typedRef={setInstruction1Typed}
+                strings={["This part of the test uses a different kind of spelling. First you will hear a word."]}
+                typeSpeed={30}
+                className='tutorial-text'
+                stopped
+                showCursor={false}
+                onStart={() => setIsAnimating(true)}
+                onComplete={() => setIsAnimating(false)}
           />
-          <div>
+
+          <audio ref={instructionAudioRef1} src={instruction1Path} onPlay={()=> setIsPlaying(true)} onEnded={()=> setIsPlaying(false)} />
+          { animationPhase > 0 &&
+          <div style={{ position: 'relative', display: 'inline-block' }}>
+            <audio ref={sampleAudioRef} src={samplePath} controls style={{ position: 'relative', zIndex: 1 }} onPlay={()=> setIsPlaying(true)} onEnded={()=> setIsPlaying(false)}/>
+            <div
+              ref={playButtonRef}
+              style={{
+                position: 'absolute',
+                top: '0',
+                left: '0',
+                right: '0',
+                bottom: '0',
+                zIndex: animationPhase > 3 ? -1 : 2,
+              }}
+            ></div>
+          </div>
+          }
+          <ReactTyped
+                typedRef={setInstruction2Typed}
+                strings={['You can replay this word by pressing the play button.<br> Next you should write this word exactly the way it sounds. You do not need to worry about the correct spelling. <br> For example, the word "laugh" can be spelled "laf". <br> Please spell the word "laugh" as demonstrated']}
+                typeSpeed={30}
+                className='tutorial-text'
+                stopped
+                showCursor={false}
+                onStart={() => setIsAnimating(true)}
+                onComplete={() => setIsAnimating(false)}
+          />
+          <audio ref={instructionAudioRef2} src={instruction2Path} onPlay={()=> setIsPlaying(true)} onEnded={()=> setIsPlaying(false)} />
+          
+          { animationPhase > 2 &&
+          <ReactTyped
+            typedRef={setInputTyped}
+            strings={["laf",""]}
+            typeSpeed={250}
+            backSpeed={70}
+            attr="value"
+            stopped
+            onStart={() => setIsAnimating(true)}
+            onComplete={() => setIsAnimating(false)}
+          >
             <input
               type="text"
-              placeholder="Enter spelling"
-              value={userInputs[currentItem]}
+              placeholder="Enter Spelling"
+              value={sampleInputValue}
               spellCheck={false}
-              autoCapitalize="none"
-              autoComplete="off"
               autoCorrect="off"
+              autoComplete="off"
+              autoCapitalize="none"
               inputMode="none"
               autoFocus
-              onChange={(e) => {
-                const newInputs = [...userInputs];
-                newInputs[currentItem] = e.target.value;
-                setUserInputs(newInputs);
-              }}
+              onChange={(e) => setSampleInputValue(e.target.value)}
             />
+          </ReactTyped>
+          }
+          { animationPhase > 4 &&
+            <button disabled={!["laf","laaf"].includes(sampleInputValue.toLowerCase().trim()) } onClick={() => setIsTutorial(false)}>
+              Start The Test
+            </button>
+          }
+        </Stack>
+      }
+      { !isTutorial &&
+      <>
+        <div>
+          <h1>Please spell these words exactly how they sound. <br></br> Do not worry about correct spelling.</h1>
+          {/* <p>For instance, laugh should be spelled 'laf'.</p> */}
+        </div>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleSubmit();
+          }}
+        >
+          <div>
+            {currentItem === 0 && (
+              <div>
+                <h3>Press Play</h3>
+              </div>
+            )}
+            <audio
+              src={audioPaths[currentItem]}
+              controls
+              autoPlay={currentItem !== 0}
+            />
+            <div>
+              <input
+                type="text"
+                placeholder="Enter spelling"
+                value={userInputs[currentItem]}
+                spellCheck={false}
+                autoCapitalize="none"
+                autoComplete="off"
+                autoCorrect="off"
+                inputMode="none"
+                autoFocus
+                onChange={(e) => {
+                  const newInputs = [...userInputs];
+                  newInputs[currentItem] = e.target.value;
+                  setUserInputs(newInputs);
+                }}
+              />
+            </div>
           </div>
-        </div>
-        <div className="button-container">
-          {incompleteSubmit && <p>Please answer this item.</p>}
-          <button type="submit">
-            {currentItem < audioPaths.length - 1 ? "Next" : "Submit"}
-          </button>
-        </div>
-      </form>
+          <div className="button-container">
+            {incompleteSubmit && <p>Please answer this item.</p>}
+            <button type="submit">
+              {currentItem < audioPaths.length - 1 ? "Next" : "Submit"}
+            </button>
+          </div>
+        </form>
+      </>
+      }
     </div>
   );
 };
