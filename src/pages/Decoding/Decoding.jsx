f3a8893054fb281d482eadb35eaa3933c0cdab28
griff <griff1293@gmail.com>
2025-02-27 20:03:00 -0600

    add tutorial component

--- src/pages/Decoding/Decoding.jsx
+++ src/pages/Decoding/Decoding.jsx
@@ -6,6 +6,7 @@ import { useCountdown } from '../../hooks/useCountdown';
 import ProgressBar from '../../components/ProgressBar';
 import './Decoding.css';
 import { errorMessages } from '../../utils/constants';
+import DecodingTutorial from './DecodingTutorial';
 
 const Decoding = () => {
 
@@ -25,6 +26,7 @@ const Decoding = () => {
   const [speechResultReceived, setSpeechResultReceived] = useState(false);
   const [nextDecodingWord, currentWord, isLastWord, progress] = useHandleDecodingLogic({ startCountdown, stopCountdown, countdownPromise, countdownFinished, setSpeechResultReceived, setRetryMessage, isPaused, setButtonActive, setError: setTranscribingError, setIsTranscribing, setIsStarted, audioStream, setUserHasSpoken, userHasSpoken });
   const [buttonDisable, setButtonDisable] = useState(false);
+  const [isTutorial, setIsTutorial] = useState(true);
 
 
   useEffect(() => {
@@ -95,60 +97,64 @@ const Decoding = () => {
 
   return (
     <div className="centered-content">
-      <div className='decoding-page'>
-        <div className="decoding-content">
-          {!isPaused && isStarted && !isLastWord && buttonActive && !isTranscribing && !transcribingError ? (
+      {isTutorial ? (
+        <DecodingTutorial setIsTutorial={setIsTutorial} />
+      ) : (
+        <div className='decoding-page'>
+          <div className="decoding-content">
+            {!isPaused && isStarted && !isLastWord && buttonActive && !isTranscribing && !transcribingError ? (
+                    <div className="button-container">
+                      <div>
+                        {retryMessage && <p>{retryMessage}</p>}
+                        <button disabled={buttonDisable} onClick={nextDecodingWord}>{retryMessage ? 'Try Again' : 'Next Word'}</button>
+                      </div>
+                    </div>
+                  ) : null }
+            {!isStarted && !transcribingError && (
                   <div className="button-container">
                     <div>
-                      {retryMessage && <p>{retryMessage}</p>}
-                      <button disabled={buttonDisable} onClick={nextDecodingWord}>{retryMessage ? 'Try Again' : 'Next Word'}</button>
+                    <button disabled={buttonDisable} onClick={startDecoding}>Start</button>
                     </div>
                   </div>
-                ) : null }
-          {!isStarted && !transcribingError && (
-                <div className="button-container">
-                  <div>
-                  <button disabled={buttonDisable} onClick={startDecoding}>Start</button>
+                )}
+            {transcribingError && (
+                  <div className="button-container">
+                    
+                    {transcribingError == errorMessages.EPMTY_TRANSCRIPTION ? 
+                      (
+                        <div>
+                          <p className='custom-p-error'>{transcribingError}</p>
+                          <button onClick={nextDecodingWord}>Try Again</button>
+                        </div>
+                        )
+                      : (
+                        <div>
+                          <p className='custom-p-error'>It seems your connection to the server has a problem. You can try to recoonect.</p>
+                          <button  onClick={nextDecodingWord}>Retry Connection</button>
+                        </div>
+                        )
+                    }
+                    
                   </div>
-                </div>
-              )}
-          {transcribingError && (
-                <div className="button-container">
-                  
-                  {transcribingError == errorMessages.EPMTY_TRANSCRIPTION ? 
-                    (
-                      <div>
-                        <p className='custom-p-error'>{transcribingError}</p>
-                        <button onClick={nextDecodingWord}>Try Again</button>
-                      </div>
-                      )
-                    : (
-                      <div>
-                        <p className='custom-p-error'>It seems your connection to the server has a problem. You can try to recoonect.</p>
-                        <button  onClick={nextDecodingWord}>Retry Connection</button>
-                      </div>
-                      )
-                  }
-                  
-                </div>
-              )}
-          {isTranscribing && !transcribingError && (
-                <div className="button-container">
-                  <div>
-                  <button disabled={true} >Analyzing...</button>
+                )}
+            {isTranscribing && !transcribingError && (
+                  <div className="button-container">
+                    <div>
+                    <button disabled={true} >Analyzing...</button>
+                    </div>
                   </div>
-                </div>
-              )}
-          {!buttonActive && isStarted && !speechResultReceived &&  !isTranscribing && !transcribingError && (
-                <>
-                  <p className='custom-p'>Say this word: </p>
-                  <h2 className='custom-h2'>{currentWord}</h2>
-                  <AudioVisualizer isActive={(true)} setUserHasSpoken={setUserHasSpoken} userHasSpoken={userHasSpoken} audioAnalyser={audioAnalyser} countdownValue={count} size="300"/>
-                </>
-              )}
+                )}
+            {!buttonActive && isStarted && !speechResultReceived &&  !isTranscribing && !transcribingError && (
+                  <>
+                    <p className='custom-p'>Say this word: </p>
+                    <h2 className='custom-h2'>{currentWord}</h2>
+                    <AudioVisualizer isActive={(true)} setUserHasSpoken={setUserHasSpoken} userHasSpoken={userHasSpoken} audioAnalyser={audioAnalyser} countdownValue={count} size="300"/>
+                  </>
+                )}
+          </div>
+          <ProgressBar progress={progress} />
         </div>
-        <ProgressBar progress={progress} />
-      </div>
+      )}
     </div>
   );
 };
